b66680d420c3fc2ca750023c9c16d6cd286c2fb6
mikecat08 <kyorokyoro0813@gmail.com>
2022-12-08 19:01:21 +0900

    問い合わせ完了画面追加

--- flaskbook/apps/minimalapp/app.py
+++ flaskbook/apps/minimalapp/app.py
@@ -1,5 +1,5 @@
 # Flaskクラスをimportする
-from flask import Flask
+from flask import Flask, current_app, g, render_template, request, url_for, redirect
 
 
 # Flaskクラスをインスタンス化する
@@ -17,3 +17,37 @@ def index():
     endpoint="hello-endpoint")
 def hello(name):
     return f"Hello, {name}!"
+
+
+@app.route("/name/<name>")
+def show_name(name):
+    # 変数をテンプレートエンジンに渡す
+    return render_template("index.html", name=name)
+
+
+with app.test_request_context():
+    # /
+    print(url_for("index"))
+    # /hello/world
+    print(url_for("hello-endpoint", name="world"))
+    # /name/ichiro?page=1
+    print(url_for("show_name", name="ichiro", page="1"))
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/contact/complete", methods=["GET", "POST"])
+def contact_complete():
+    if request.method == "POST":
+        # form属性を使ってフォームの値を取得する
+        username = request.form["username"]
+        email = request.form["email"]
+        description = request.form["description"]
+        # メールを送る（最後に実装）
+
+        # contactエンドポイントへリダイレクトする
+        return redirect(url_for("contact_complete"))
+
+    return render_template("contact_complete.html")
