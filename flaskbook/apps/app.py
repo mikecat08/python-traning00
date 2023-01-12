a69f88d43a52aba0aa878717a6f7d10fc4d2929f
mikecat08 <kyorokyoro0813@gmail.com>
2023-01-12 14:17:12 +0900

    認証機能の登録とエンドポイント作成

--- flaskbook/apps/app.py
+++ flaskbook/apps/app.py
@@ -1,27 +1,20 @@
-from pathlib import Path
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
-
+from apps.config import config
 # SQLAlchemyをインスタンス化する
 db = SQLAlchemy()
 
 csrf = CSRFProtect()
 
 # create_app関数を作成する
-def create_app():
+def create_app(config_key):
   # Flaskインスタンス生成
   app = Flask(__name__)
-  # アプリのコンフィグ設定をする
-  app.config.from_mapping(
-    SECRET_KEY="2AZSMss3p5QPbcY2hBsJ",
-    SQLALCHEMY_DATABASE_URI=f"sqlite:///{Path(__file__).parent.parent / 'local.sqlite'}",
-    SQLALCHEMY_TRACK_MODIFICATIONS=False,
-    # SQLをコンソールログに出力する設定
-    SQLALCHEMY_ECHO=True,
-    WTF_CSRF_SECRET_KEY="AuwzyszU5sugKN7KZs6f",
-  )
+
+  # config_keyにマッチする環境のコンフィグクラスを読み込む
+  app.config.from_object(config[config_key])
 
   csrf.init_app(app)
 
@@ -35,5 +28,11 @@ def create_app():
 
   # register_blueprintを使いviewsのcrudをアプリへ登録する
   app.register_blueprint(crud_views.crud, url_prefix="/crud")
+
+  # authパッケージからviewsをimportする
+  from apps.auth import views as auth_views
+
+  # register_blueprintを使いviewsのauthをアプリへ登録する
+  app.register_blueprint(auth_views.auth, url_prefix="/auth")
 
   return app
